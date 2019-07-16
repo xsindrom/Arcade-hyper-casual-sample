b782f61a1385adb12a19c730723f922641cb8849
xsindrom <xsindrom.force@gmail.com>
2019-07-16 11:21:58 +0300

    Add files via upload

--- GameController.cs
+++ GameController.cs
@@ -1,40 +1,202 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Currency;
+using Currencies;
 using Storage;
+using Units;
+using Battle;
+using Rewards;
+using Network.GameServer;
+using Cards;
+using SpecialEvents;
+using Utilities.Localization;
+using System;
 
 public class GameController : MonoSingleton<GameController>
 {
-    public CurrencyController CurrencyController { get; private set; }
-    public StorageController StorageController { get; private set; }
+    #region Controllers
+    private CurrencyController currencyController;
+    private StorageController storageController;
+    private ChestsController chestsController;
+    private CardDataController cardDataController;
+    private SpecialEventsController specialEventsController;
+    private ArenaController arenaController;
+    private UserExperienceController userExperienceController;
 
-    public override void Init()
+    public CurrencyController CurrencyController
+    {
+        get { return currencyController; }
+    }
+
+    public StorageController StorageController
+    {
+        get { return storageController; }
+    }
+
+    public ChestsController ChestsController
+    {
+        get { return chestsController; }
+    }
+
+    public CardDataController CardDataController
+    {
+        get { return cardDataController; }
+    }
+
+    public SpecialEventsController SpecialEventsController
+    {
+        get { return specialEventsController; }
+    }
+
+    public UserExperienceController UserExperienceController
+    {
+        get { return userExperienceController; }
+    }
+
+    public ArenaController ArenaController
+    {
+        get { return arenaController; }
+    }
+    #endregion
+
+    private UnitDatabase unitDatabase;
+    public UnitDatabase UnitDatabase
+    {
+        get
+        {
+            if (!unitDatabase)
+            {
+                unitDatabase = AssetBundlesLoader.Instance.GetAsset<UnitDatabase>(Constants.UNITS_BUNDLE, Constants.ASSET_UNITS_DATABASE);
+            }
+            return unitDatabase;
+        }
+    }
+
+    private RewardsStorage rewardsStorage;
+    public RewardsStorage RewardsStorage
+    {
+        get
+        {
+            if (!rewardsStorage)
+            {
+                rewardsStorage = AssetBundlesLoader.Instance.GetAsset<RewardsStorage>(Constants.SETTINGS_BUNDLE, Constants.ASSET_REWARDS_STORAGE);
+            }
+            return rewardsStorage;
+        }
+    }
+
+    private SpeedStatRangeContainer rangeContainer;
+    public SpeedStatRangeContainer RangeContainer
+    {
+        get
+        {
+            if (!rangeContainer)
+            {
+                rangeContainer = AssetBundlesLoader.Instance.GetAsset<SpeedStatRangeContainer>(Constants.UNITS_BUNDLE, Constants.ASSET_SPEED_STAT_RANGES);
+            }
+            return rangeContainer;
+        }
+    }
+
+    protected override void Init()
     {
         base.Init();
-        StorageController = new StorageController();
-        CurrencyController = new CurrencyController();
+#if UNITY_EDITOR || UNITY_STANDALONE
+        //TEST
+        Screen.SetResolution(304, 540, false);
+        ///
+#endif
 
-        StorageController.Load();
-    }
+        Application.targetFrameRate = 60;
 
-    protected override void OnApplicationPause(bool pause)
-    {
-        base.OnApplicationPause(pause);
-        if (pause)
-            StorageController.Save();
-    }
+        storageController = new StorageController();
 
-    protected override void OnApplicationQuit()
-    {
-        base.OnApplicationQuit();
-        StorageController.Save();
+        GooglePlayServices.Activate();
+        FacebookSDK.Init();
+
+        GameServer.Instance.Login(OnLogin);
     }
 
     protected override void OnDestroy()
     {
-        CurrencyController?.Dispose();
-        StorageController?.Dispose();
+        
+        chestsController?.Dispose();
+        cardDataController?.Dispose();
+        specialEventsController?.Dispose();
+        arenaController?.Dispose();
+        userExperienceController?.Dispose();
+        storageController?.Dispose();
+        currencyController?.Dispose();
+
+        chestsController = null;
+        cardDataController = null;
+        specialEventsController = null;
+        arenaController = null;
+        userExperienceController = null;
+        storageController = null;
+
+        currencyController = null;
+        AssetBundlesLoader.UnSubscribeOnBundlesLoaded(this);
         base.OnDestroy();
+    }
+
+    private void OnLogin(GameServerLoginResponce loginResponse)
+    {
+        if (loginResponse.status == Status.OK)
+        {
+            GameServer.Instance.GetSaveData(OnGetSaveData);
+        }
+        else
+        {
+            InitBySaveData(loginResponse);
+        }
+    }
+
+    private void OnGetSaveData(GameServerGetSaveResponse response, SaveData saveData)
+    {
+        if (response.status == Status.OK)
+        {
+            if (saveData != null)
+            {
+                storageController.Data = saveData;
+            }
+            else
+            {
+                storageController.Load();
+                GameServer.Instance.UpdateSaveData(InitBySaveData);
+                return;
+            }
+        }
+        InitBySaveData(response);
+    }
+
+    private void InitBySaveData(GameServerBaseResponse response)
+    {
+        AssetBundlesLoader.SubscribeOrGetOnBundlesLoaded(this,OnSettingsBundleLoaded, Constants.SETTINGS_BUNDLE);
+    }
+
+    private void OnSettingsBundleLoaded(string[] bundles)
+    {
+        Teams.TeamsController.CreateInstance(true);
+
+        currencyController = new CurrencyController();
+        chestsController = new ChestsController();
+        cardDataController = new CardDataController();
+        specialEventsController = new SpecialEventsController();
+        userExperienceController = new UserExperienceController();
+        arenaController = new ArenaController();
+
+        storageController.Load();
+
+        Language.Instance.LangCode = (LangCode)Enum.Parse(typeof(LangCode), storageController.Data.UserData.Language.Value);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            storageController.Save();
+            GameServer.Instance.UpdateSaveData(InitBySaveData);
+        }
     }
 }
